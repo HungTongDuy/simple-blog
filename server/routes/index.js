b48a9f1302a7d2df579b8c100100b385fc21aae3
Hung Tong <tongduyhung9x@gmail.com>
2018-08-09 00:14:39 +0700

    add module json web token

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+var jwt    = require('jsonwebtoken'); // used to create, sign, and verify tokens
 
 router.use('/api', require('./api'));
 
